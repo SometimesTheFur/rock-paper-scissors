fcf077d7ed51d3dcce3744494f475e807f459f04
SometimesTheFur <timothymahr@gmail.com>
2020-05-09 15:56:06 -0400

    Made buttons clickable

--- rps.js
+++ rps.js
@@ -1,51 +1,61 @@
-        
+const shoot = ["rock", "paper", "scissors"];   
+let playerSelection = '';
+
+    const rockBtn = document.querySelector('#rock');
+        rockBtn.addEventListener('click', function() {
+            playerSelection = shoot[0];
+            return playerSelection;
+        });
+
+    const paperBtn = document.querySelector('#paper');
+        paperBtn.addEventListener('click', function() {
+            playerSelection = shoot[1];
+            return playerSelection;
+        });
+
+    const scissorsBtn = document.querySelector('#scissors');
+        scissorsBtn.addEventListener('click', function() {
+            playerSelection = shoot[2];
+            return playerSelection;
+        });
 
 
-function player() {
-    const btnRock = document.querySelector('rock');
-    const btnPaper = document.querySelector('paper');
-    const btnScissors = document.querySelector('scissors');
-      
-}
-   document.getElementById('rock').addEventListener('click', playRound);
-   document.getElementById('paper').addEventListener('click', playRound);
-   document.getElementById('scissors').addEventListener('click', playRound);
-    
-    const shoot = ["rock", "paper", "scissors"];
     function computerPlay() {
-        return shoot[Math.floor(Math.random() * 3)];
+        let rand = shoot[Math.floor(Math.random() * 3)];
+        return rand;
     }
-    
-    const computerSelection = computerPlay();
+
     let scoreP = 0;
     let scoreC = 0;
-
+    
+        let computerSelection = '';
     function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && computerSelection === "rock") {
+        
+        if (playerSelection === "rock" && computerPlay === "rock") {
             return "Tie! Rock is equal to Rock!"+
                 "   Score: " + (scoreP + "-" + scoreC);
-        } else if (playerSelection === "rock" && computerSelection === "paper") {
+        } else if (playerSelection === "rock" && computerPlay === "paper") {
             return "You Lose! Paper beats Rock!"+
                 "   Score: " + (scoreP + "-" + ++scoreC);
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        } else if (playerSelection === "rock" && computerPlay === "scissors") {
             return "You win! Rock beats Scissors!"+
                 "   Score: " + (++scoreP + "-" + scoreC);
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
+        } else if (playerSelection === "paper" && computerPlay === "rock") {
             return "You win! Paper beats Rock!"+
                 "   Score: " + (++scoreP + "-" + scoreC);
-        } else if (playerSelection === "paper" && computerSelection === "paper") {
+        } else if (playerSelection === "paper" && computerPlay === "paper") {
             return "Tie! Paper is equal to Paper!"+
                 "   Score: " + (scoreP + "-" + scoreC);
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        } else if (playerSelection === "paper" && computerPlay === "scissors") {
             return "You Lose! Scissors beat Paper!"+ 
                 "   Score: " + (scoreP + "-" + ++scoreC);
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        } else if (playerSelection === "scissors" && computerPlay === "rock") {
             return "You Lose! Rock beats Scissors!"+
                 "   Score: " + (scoreP + "-" + ++scoreC);
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        } else if (playerSelection === "scissors" && computerPlay === "paper") {
             return "You win! Scissors beat Paper!"+
                 "   Score: " + (++scoreP + "-" + scoreC);
-        } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        } else if (playerSelection === "scissors" && computerPlay === "scissors") {
             return "Tie! Scissors are equal to Scissors!"+
                 "   Score: " + (scoreP + "-" + scoreC);
         } else {
@@ -53,9 +63,20 @@ function player() {
         }
     }
 
+
+
+    const play = document.querySelector('button');
+        play.addEventListener('click', function(e){
+   play.onclick(playRound(playerSelection,computerSelection))
+   });
+
+    const round = document.querySelector('#round');
+    round.innerText = playRound(playerSelection, computerSelection);
+
+
     //function playGame() {
         //for (let i = 1; i < 6; ++i) {
-            //console.log("Round " + i + ". " + playRound(playerSelection, computerSelection)); 
+            //console.log("Round " + i + ". " + playRound(playerSelection, computerPlay)); 
         //}
     //}
     function finalScore() {
@@ -67,7 +88,5 @@ function player() {
                 return "Final Score: " + (scoreP + "-" + scoreC) + "   Not much to say, it was a tie. Try again!";
             }
     }
+        //document.addEventListener('click', finalScore); 
     //console.log(playGame())+
-    const round = document.querySelector('#round');
-    document.addEventListener('click', finalScore);
-    document.getElementById('round').innerHTML = playRound();
